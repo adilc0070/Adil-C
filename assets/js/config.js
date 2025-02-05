a99c0146cb5b7cfd279e8c1d43df2960bbb876e1
Adil C <adilc0070@gmail.com>
2025-02-05 13:50:37 +0530

    fixes

---
+++ assets/js/config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwreSeOA98wqOXBmvjZajWYIteKtEN5wBD8-qbfj0dVzGY5Ha5HJqBejYaI4HjRufP9/exec",
+    API_URL: "https://script.google.com/macros/s/AKfycbwreSeOA98wqOXBmvjZajWYIteKtEN5wBD8-qbfj0dVzGY5Ha5HJqBejYaI4HjRufP9/exec"
+};
+  
